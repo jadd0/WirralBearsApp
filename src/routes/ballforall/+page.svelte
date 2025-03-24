4bf2cf5d8da4535527f16ed01cfcaed714757f02
Jadd Al-Khabbaz <jaddalkhabbaz@gmail.com>
2025-03-24 15:29:00 +0000

    ball4all fine for now

---
+++ src/routes/ballforall/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+
+</script>
+
+<svelte:head>
+  <title>Ball4All</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicon_io/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon_io/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon_io/favicon-16x16.png">
+  <link rel="manifest" href="/favicon_io/site.webmanifest">
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:300,500" rel="stylesheet">
+</svelte:head>
+
+
+
+<br>
+<a href="/">
+  <img src="/images/bears b star1.png" class="bear" alt="Bears logo"/>
+</a>
+
+<h1>We ask for one commitment – Ball for All</h1>
+
+<div class="container">
+  <p>If anyone (player or coach) wants to be involved in the youth club, they must commit to the Ball for All principles and hold everyone to account for them. They create great and enjoyable basketball.
+
+    To join, a player must discuss these principles with the coaches and team-mates, sign them, and play by them.
+    No player will be asked to leave the club unless they do not follow these principles:</p>
+</div>
+
+<div class="box">
+  <div class="innerBox">
+    1) Joy – it's the essence of success<br>
+    2) Positivity – positive mental attitude is key<br>
+    3) Respect – we treat others like we would like to be treated<br>
+    4) Equal Focus and Equal Minutes – we are all part of this team<br>
+    5) Dedication and effort – It's how we get better<br>
+    6) Growth – It's where we are now, and where we will be in the future<br>
+    7) Pass – because ball hogging ruins the game<br>
+    8) Defence – It wins championships<br>
+    9) Shoot – when it's good and within range. The only miss is not taking a good shot<br>
+    10) Play – together as a team.<br>
+  </div>
+</div>
+
+<p class="B4A">Because it's ball for all, not just some.</p>
+
+<div class="footer">
+  <div class="footerBox">
+    <div class="getInTouchBox">
+      <h3>Get In Touch</h3>
+      <li> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jaddalkwork@gmail.com">Website Designer: Jadd (jaddalkwork@gmail.com)</a></li><br>
+      <li> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=icona.photo.service@gmail.com">Photographer: Giannis (icona.photo.service@gmail.com)</a></li><br>
+      <li> <a href="https://mail.google.com/mail/?view=cm&fs=1&to=wirralbears@gmail.com">Club (wirralbears@gmail.com)</a></li>
+    </div>
+
+    <div class="middle"><img src="/images/bears big red.png" alt="Bears logo" class="bearFooter"></div>
+
+    <div class="moreInfoBox">
+      <h3>More Info</h3>
+      <li><a href="https://goo.gl/maps/2dHvRTcbWPFoZLoT6">Woodchurch Location</a></li><br>
+      <li><a href="https://goo.gl/maps/Zs2cBJ1htbrRkEcY6">Neston Location</a></li><br>
+      
+      <div class="socials">
+        <a href="https://facebook.com/WirralBears/"><img src="/images/facebook.png" alt="Facebook" class="facebook"></a>
+        <a href="https://instagram.com/wirral_bears_basketball_club?r=nametag"><img src="/images/instagram.png" alt="Instagram" class="insta"></a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="sponsorsDiv">
+    <a href="https://taylorbrownsolicitors.com" class="holder">
+      <img src="/images/taylor Brown 1.png" alt="Taylor Brown Solicitors" class="tb" />
+    </a>
+    <a href="https://www.chesterfinancial.co.uk" class="holder">
+      <img src="/images/Chester Financial.png" alt="Chester Financial" class="cf" />
+    </a>
+  </div>
+</div>
+
+<style>
+
+</style>
